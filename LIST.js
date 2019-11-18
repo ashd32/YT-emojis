cd8ea3a5ab040d05f2869656212098528563c228
Ashley Donald <ashd32@gmail.com>
2019-11-18 16:51:24 -0500

    adds files

---
+++ LIST.js
@@ -0,0 +1,40 @@
+Light Bright T 💫💫💫💫💫
+Lioness of Judah 🦁🦁🦁🦁🦁
+LovelyTi (Tea Sippers) ☕️☕️☕️☕️☕️
+M-Bernadette (The House Of Luvs)  🏩🏩🏩🏩🏩
+Mature*ISH (The Tribe)  🌎🌎🌎🌎🌎
+Michelle ATLien Brown 👽👽👽👽👽
+Mimi Checkin Fake Heaux (Checks) ✔️✅✔️✅✔️✅✔️✅✔️✅
+Mona Symone (The Brains) 🧠🧠🧠🧠🧠
+Mrs.Diva1 (Smoooches)  💋💋💋💋💋
+Nicole_Reloaded 🎲🎲🎲🎲🎲
+Nicole Tyler 🎤🎤🎤🎤🎤
+Nique at Night 🌙🌙🌙🌙🌙
+NoSaltBP (We Are Not Salty)️  ❤️❤️❤️❤️❤️
+Onsight April (April's Arsenals) 💣💥💣💥💣💥💣💥💣💥
+PG&SJ (The Homies)  👓💪🏽👓💪🏽👓💪🏽👓💪🏽👓💪🏽
+Raw Honest Tea (Butterfly Army) 🦋🦋🦋🦋🦋
+Seven Stars (Girl Cave) ⭐️🌟 .  ⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+Sexyharlem125 (The Purple Heartz) 💜💜💜💜💜
+ShaNTasSTicC (The Trill Ones) 💯💯💯💯💯
+Shawn Bradley 🦁👑🦁👑🦁👑🦁👑🦁👑
+Tanya's Primetime TV  📺📺📺📺📺
+Tasha K (Winos) 🍷🍷🍷🍷🍷
+The Wiley Show 👁👁👁👁👁
+THE WRENCH 🔧🔧🔧🔧🔧
+Tracie's World  🗺🗺🗺🗺🗺
+Treecey Living My Truth (Treecey's Pieces) 🧩🍬🧩🍬🧩🍬🧩🍬🧩🍬
+THE TRUTH (THE TRUTH ENTERPRISE) 🕋🕋🕋🕋🕋
+Truth Slayer Saying it Vlog ⚔️⚔️⚔️⚔️⚔️
+VS 🆚🆚🆚🆚🆚
+
+JAMES CALDWELL
+NAY ROB
+J WILSON
+ZERO TOLERANCE
+RICK REED
+RITA
+S HUTCHINSON
+QB
+LALA
+TRISH
